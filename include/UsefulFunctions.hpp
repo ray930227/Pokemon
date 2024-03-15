d357a53a7932d2929fe58ee91e7bbb59eba8a8e9
unknown <ray>
2024-03-15 10:23:17 +0800

    add UsefulFunction

---
+++ include/UsefulFunctions.hpp
@@ -0,0 +1,10 @@
+#ifndef POKEMON_USEFULFUNCTIONS_HPP
+#define POKEMON_USEFULFUNCTIONS_HPP
+
+#include <vector>
+#include <string>
+
+namespace useful {
+    std::vector<std::string> split(std::string& str,char interval);
+}
+#endif //POKEMON_USEFULFUNCTIONS_HPP
---
+++ src/UsefulFunction.cpp
@@ -0,0 +1,12 @@
+#include "UsefulFunctions.hpp"
+
+std::vector<std::string> useful::split(std::string &str, char interval) {
+    std::vector<std::string> result;
+    str+=" ";
+    size_t cur=0;
+    while (cur<str.size()){
+        result.push_back(str.substr(str.find(interval,cur)));
+        cur=str.find(interval,cur)+1;
+    }
+    return result;
+}
