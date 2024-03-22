ca73bce48be3b1a8c86dbca78a352bac01e144af
unknown <ray>
2024-03-22 09:24:20 +0800

    add PokemonBag

---
+++ include/PokemonBag.hpp
@@ -0,0 +1,19 @@
+#ifndef POKEMON_POKEMONBAG_HPP
+#define POKEMON_POKEMONBAG_HPP
+
+#include "Pokemon.hpp"
+
+class PokemonBag {
+private:
+    std::vector<std::shared_ptr<Pokemon>> Pokemons;
+public:
+    PokemonBag();
+
+    std::vector<std::shared_ptr<Pokemon>> GetPokemons();
+
+    void addPomekon(const std::shared_ptr<Pokemon>& pokemon);
+
+    bool isFull();
+};
+
+#endif //POKEMON_POKEMONBAG_HPP
---
+++ src/PokemonBag.cpp
@@ -0,0 +1,18 @@
+#include "PokemonBag.hpp"
+
+PokemonBag::PokemonBag() {
+
+}
+
+std::vector<std::shared_ptr<Pokemon>> PokemonBag::GetPokemons() {
+    return Pokemons;
+}
+
+void PokemonBag::addPomekon(const std::shared_ptr<Pokemon>& pokemon) {
+    if(!isFull())
+        Pokemons.push_back(pokemon);
+}
+
+bool PokemonBag::isFull() {
+    return Pokemons.size() == 6;
+}
