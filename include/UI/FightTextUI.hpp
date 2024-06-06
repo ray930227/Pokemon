69e2401e12588f94441413220e9b6b78306b5651
howe <t111590018@ntut.org.tw>
2024-06-06 21:22:28 +0800

    Add Win Text and SetLevel

--- include/UI/FightTextUI.hpp
+++ include/UI/FightTextUI.hpp
@@ -5,26 +5,46 @@
 
 class FightTextUI {
 private:
-    std::shared_ptr<TextBox> m_FinishBox;
     std::shared_ptr<TextBox> m_PlayerTextBox;
     std::shared_ptr<TextBox> m_EnemyTextBox;
     std::shared_ptr<TextBox> m_DefeatTextBox;
+    std::shared_ptr<TextBox> m_LoseTextBox;
+    std::shared_ptr<TextBox> m_FinishTextBox;
+    std::shared_ptr<TextBox> m_GainEXPTextBox;
+    std::shared_ptr<TextBox> m_RunTextBox;
+    std::shared_ptr<TextBox> m_LevelUPTextBox;
 public:
     FightTextUI();
 
     [[nodiscard]] std::vector<std::vector<std::shared_ptr<Util::GameObject>>> GetChildren() const;
 
-    void SetFinish(const std::string &Winner,const std::string &Loser);
-
-    void SetDefeat(const std::string &PokeName);
-
     void SetPlayer(const std::string &PlayerName, const std::string &UseSkill, float DamageRate);
 
     void SetEnemy(const std::string &EnemyName, const std::string &UseSkill, float DamageRate);
 
+    void SetDefeat(const std::string &PokeName);
+
+    void SetLose(const std::string &PlayerName);
+
+    void SetFinish(const std::string &Winner,const std::string &Loser);
+
+    void SetGainEXP(const std::string &PokeName,int EXP);
+
+    void SetRun();
+
+    void SetLevelUP(const std::string &PokeName,int NewLV);
+
     [[nodiscard]] bool GetPlayerVisibility() const;
 
     [[nodiscard]] bool GetEnemyVisibility() const;
+
+    [[nodiscard]] bool GetDefeatVisibility() const;
+
+    [[nodiscard]] bool GetGainEXPVisibility() const;
+
+    [[nodiscard]] bool GetRunVisibility() const;
+
+    [[nodiscard]] bool GetLevelUPVisibility() const;
 
     void Next();
 };
