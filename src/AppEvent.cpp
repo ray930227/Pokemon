ef87c228d8db4259acc5a07602253e839ab80474
unknown <ray>
2024-06-07 23:04:27 +0800

    fix conflict

--- src/AppEvent.cpp
+++ src/AppEvent.cpp
@@ -139,6 +139,7 @@ void App::Event() {
     } else if (m_CurrentEvent == EventID::GRASS) {
         //region
         if (encounterable && rand() % 100 < 20) {
+            isWildPokemon = true;
             Enemy=std::make_shared<Character>();
             std::vector<int> tempID={ 13, 16, 19, 21, 23, 25, 26, 27, 29, 31, 32, 34, 35, 36, 37, 38, 39, 40, 41, 43, 45, 46, 48, 50, 52, 54, 56, 58, 59, 60, 62, 63, 65, 66, 68, 69, 71, 72, 74, 76, 77, 79, 81, 83, 84, 86, 88, 90, 91, 92, 94, 95, 96, 98, 100, 102, 103, 104, 106, 107, 108, 109, 111, 113, 114, 115, 116, 118, 120, 121, 122, 124, 125, 126, 127, 128, 129, 131, 132};
             int r=rand()%tempID.size();
