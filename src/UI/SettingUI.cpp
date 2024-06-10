56ecfe6235fefc723259874f5a2ad9dd8ec7f878
unknown <ray>
2024-06-10 16:36:52 +0800

    not have pokemon cannot open bag

--- src/UI/SettingUI.cpp
+++ src/UI/SettingUI.cpp
@@ -12,6 +12,10 @@ SettingUI::SettingUI(const std::shared_ptr<Character>& Player) {
     m_PokeBagUI=std::make_shared<PokeBagUI>(Player);
     m_PokeBagUI->SetVisible(false);
 
+    m_TB=std::make_shared<TextBox>();
+    m_TB->SetZIndex(53);
+    m_TB->SetVisible(false);
+
     m_Player=Player;
 }
 
@@ -22,14 +26,31 @@ void SettingUI::Start() {
 }
 
 void SettingUI::Run() {
-    if(m_PokeBagUI->GetVisible()){
+    if(m_TB->GetVisibility()){
+        if(Util::Input::IsKeyDown(Util::Keycode::Z)){
+            m_TB->Next();
+        }
+    } else if(m_PokeBagUI->GetVisible()){
         m_PokeBagUI->Run(3);
     } else if(Util::Input::IsKeyDown(Util::Keycode::Z)){
         switch ((int)m_Arrow->GetPosition().y) {
             case 250:
+                if(m_Player->GetItemBag()->GetItemQuantity("寶可夢圖鑑")==0){
+                    m_TB->SetVisible(true);
+                    m_TB->SetText("尚無神奇寶貝圖鑑!");
+                }
+                else{
+                    //實作圖鑑UI
+                }
                 break;
             case 178:
-                m_PokeBagUI->SetVisible(true);
+                if(m_Player->GetPokemonBag()->GetPokemons().size()==0){
+                    m_TB->SetVisible(true);
+                    m_TB->SetText("尚無任何神奇寶貝!");
+                }
+                else{
+                    m_PokeBagUI->SetVisible(true);
+                }
                 break;
             case 106:
                 break;
@@ -52,6 +73,10 @@ std::vector<std::shared_ptr<Util::GameObject>> SettingUI::GetChildren() {
     result.push_back(m_Arrow);
 
     for(auto &i:m_PokeBagUI->GetChildren()){
+        result.push_back(i);
+    }
+
+    for (auto &i: m_TB->GetChildren()) {
         result.push_back(i);
     }
 
